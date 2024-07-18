a1ebd979a1612e28e6158e1f67de03d975bbaad6
kunwoo0512 <dkzkcmzl39@khu.ac.kr>
2024-07-18 23:51:43 +0900

    10989

--- algorithm_study/src/bronze/lev1/baekjoon10989.java
+++ algorithm_study/src/bronze/lev1/baekjoon10989.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.Arrays;
 
-public class baekjoon10989 {
+public class baekjoon10989 { //백준 10989
     public static void main(String[] args) throws IOException {
 
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
